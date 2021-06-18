4425a0aa7539b0fc3f31cc8fd0a37b582ba3c2ed
SuitofSorrows <tlnestor@live.com>
2021-06-18 13:42:45 -0400

    Customized styling to improve use on mobile/small windows;

--- conn/table.php
+++ conn/table.php
@@ -20,15 +20,15 @@ echo "
         <form action='' method='post' id='new-card-form' style='display: none;'>
             <fieldset>
                 <legend>Add New Gift Card</legend>
-                <input type='number' name='gift-card-number' value='' id='new-card-input' inputmode='numeric' placeholder='Gift Card Number' required style='padding: 5px; width: 20%;'>
-                <input type='number' name='init-bal'  pattern='(\d{3})([\.])(\d{2})' min='0.00' max='999.99' step='any' value='' id='gift-number-input' data-type='currency' inputmode='numeric' placeholder='Initial Balance' required style='padding: 5px; width: 15%;'>
-                <select name='store' style='padding: 6px; width: 8%;'>
+                <input type='number' name='gift-card-number' value='' id='new-card-input' inputmode='numeric' placeholder='Gift Card Number' required>
+                <input type='number' name='init-bal'  pattern='(\d{3})([\.])(\d{2})' min='0.00' max='999.99' step='any' value='' id='gift-number-input' data-type='currency' inputmode='numeric' placeholder='Initial Balance' required>
+                <select name='store' class='store-select'>
                     <option value='PFF' selected>PFF</option>
                     <option value='LRS'>LRS</option>
                 </select>
-                <input type='text' name='initial-new' value='' id='initials-new' placeholder='Enter Initials' minlength='2' maxlength='3' pattern='[A-Za-z]{2,3}' onkeyup='this.value = this.value.toUpperCase();' oninvalid='this.setCustomValidity(`Enter 3 Letter Maximum`)' required style='padding: 5px; width: 10%;'>                
-                <input type='date' name='date-activated' value='' id='todays-date-add' style='padding: 4px; width: 160px;'>
-                <input type='submit' name='submit-new' value='ADD' style='padding: 5px; width 20%;'>
+                <input type='text' name='initial-new' value='' id='initials-new' placeholder='Initials' minlength='2' maxlength='3' pattern='[A-Za-z]{2,3}' onkeyup='this.value = this.value.toUpperCase();' oninvalid='this.setCustomValidity(`Enter 3 Letter Maximum`)' required>                
+                <input type='date' name='date-activated' value='' id='todays-date-add'>
+                <input type='submit' name='submit-new' value='ADD'>
             </fieldset>
         </form>
        ";
@@ -37,15 +37,15 @@ echo "
         <form action='' method='post' id='update-card-form' style='display: none;'>
             <fieldset>
                 <legend>Update Gift Card Balance</legend>
-                <input type='number' name='id' value='' id='selectedId' placeholder='ID' required style='padding: 5px; width: 10%;'>
-                <input type='number' name='rem-bal' pattern='(\d{3})([\.])(\d{2})' min='0.00' max='999.99' step='any' value='' id='rem-bal' data-type='currency' inputmode='numeric' placeholder='Remaining Balance' required style='padding: 5px; width: 15%;'>
-                <select name='acct-status' style='padding: 6px; width: 9%;'>
+                <input type='number' name='id' value='' id='selectedId' placeholder='ID' required>
+                <input type='number' name='rem-bal' pattern='(\d{3})([\.])(\d{2})' min='0.00' max='999.99' step='any' value='' id='rem-bal' data-type='currency' inputmode='numeric' placeholder='Remaining Balance' required>
+                <select name='acct-status' class='store-select'>
                     <option value='ACTIVE' selected>ACTIVE</option>
                     <option value='CLOSED'>CLOSED</option>
                 </select>
-                <input type='text' name='initial-up' value='' id='initials-update' placeholder='Enter Initials' minlength='2' maxlength='3' pattern='[A-Za-z]{2,3}' onkeyup='this.value = this.value.toUpperCase();' oninvalid='this.setCustomValidity(`Enter 3 Letter Maximum`)' required style='padding: 5px; width: 10%;'>
-                <input type='date' name='date-used' value='' id='todays-date' style='padding: 4px; width: 160px;'>
-                <input type='submit' name='submit-up' value='UPDATE' style='padding: 5px; width 20%;'>
+                <input type='text' name='initial-up' value='' id='initials-update' placeholder='Initials' minlength='2' maxlength='3' pattern='[A-Za-z]{2,3}' onkeyup='this.value = this.value.toUpperCase();' oninvalid='this.setCustomValidity(`Enter 3 Letter Maximum`)' required>
+                <input type='date' name='date-used' value='' id='todays-date'>
+                <input type='submit' name='submit-up' value='UPDATE'>
             </fieldset>
         </form>
        ";
@@ -54,8 +54,8 @@ echo "
         <form action='' method='post' id='history-form' style='display: none;'>
             <fieldset>
                 <legend>Look Up Card History</legend>
-                <input type='number' name='gift-card-history' id='selectedGC' value='' inputmode='numeric' placeholder='Gift Card Number' style='padding: 5px; width: 20%;'>
-                <input type='submit' name='submit-history' id='getHistory' value='GET HISTORY' style='padding: 5px; width 20%;'>
+                <input type='number' name='gift-card-history' id='selectedGC' value='' inputmode='numeric' placeholder='Gift Card Number'>
+                <input type='submit' name='submit-history' id='getHistory' value='GET HISTORY'>
             </fieldset>
         </form>
        ";
