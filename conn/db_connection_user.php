a6b92d7fb2041a4788aa5248e391bf699cbc70ad
SuitofSorrows <tlnestor@live.com>
2021-08-12 17:13:51 -0400

    Added signup and login pages that function with new db;

---
+++ conn/db_connection_user.php
@@ -0,0 +1,25 @@
+<?php
+
+// Local connection
+$mysqliSess = new MySQLi('localhost' , 'root' , '' , 'dev_gift_session');
+
+// Website connection
+//$mysqli = new MySQLi('localhost' , 'dklosygm' , 'Tomahawk7' , 'dklosygm_giftsearch');
+
+
+if(! $mysqliSess ) {
+    die('Could not connect: ' . mysql_error());
+}
+
+// Checks to see if username already exists
+if (isset($_POST['signup'])) {
+    $username = $_POST['user_name'];
+
+    $query = "SELECT * FROM users WHERE user_name='$username'";
+
+    $results = mysqli_query($mysqliSess, $query);
+
+    if (mysqli_num_rows($results) > 0) {
+        $username_error = "Sorry... username is already taken.";
+    }
+}
---
+++ conn/session_functions.php
@@ -0,0 +1,39 @@
+<?php
+
+function check_login($mysqliSess) {
+    if(isset($_SESSION['user_id'])) {
+        $id = $_SESSION['user_id'];
+        $query = "SELECT * FROM users WHERE user_id = '$id' LIMIT 1";
+
+        $result = mysqli_query($mysqliSess, $query);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+    }
+
+    // redirect to login
+
+    header("Location: includes/login.php");
+    die;
+
+}
+
+function random_num ($length) {
+
+    $text = "";
+    if ($length < 5) {
+        $length = 5;
+    }
+
+    $len = rand(4, $length);
+
+    for ($i=0; $i < $len; $i++) {
+
+        $text .= rand(0,9);
+    }
+
+    return $text;
+
+}
